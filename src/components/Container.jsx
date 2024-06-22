8f26047dc68f0cdeb6cc62d1d034893dd8cb52d4
Suresh <sureshb9a11460@gmail.com>
2024-06-22 10:26:50 +0530

    project commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import bgcofee from "../assets/bg-cafe.jpg";
+import imgstyle from "../assets/vector.svg";
+import Product from "./Product";
+
+const Container = () => {
+  const [coffee, setCoffee] = useState([]);
+
+  const [selected, setSelected] = useState(false);
+
+  const availableCofee = () => {
+    fetch(
+      "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        setCoffee(data.filter((cofee) => cofee.available === true))
+      )
+      .then(console.log("available Coffe", coffee))
+      .catch((error) => console.error(error));
+    setSelected(true);
+  };
+
+  const allproduct = () => {
+    fetch(
+      "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setCoffee(data))
+      .then(console.log("allcoffe", coffee))
+      .catch((error) => console.error(error));
+    setSelected(false);
+    console.log(fetch);
+  };
+
+  useEffect(() => {
+    allproduct();
+  }, []);
+  return (
+    <div>
+      <img
+        src={bgcofee}
+        alt="background of cofee image"
+        className=" sm:bg-cover h-[300px] w-full absolute"
+      />
+      <section className="bg-[#111315] h-full pb-40 ">
+        <div className="bg-[#1B1D1F] w-10/12 sm:w-1/2 lg:w-[90%]  lg:px-14 flex flex-col items-center mx-auto   relative  top-32 rounded-md">
+          <img
+            src={imgstyle}
+            alt="image layer"
+            className="absolute z-10 right-0"
+          />
+          <h1 className="text-white font-medium text-[30px] mx-auto mt-10 z-20">
+            Our Collection
+          </h1>
+          <p className="text-[#FEF7EE] m-5 text-[18px] z-20">
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </p>
+          <div className="flex  justify-around space-x-14 items-center">
+            <button
+              className={
+                selected === false
+                  ? "bg-[#6F757C] text-[#FEF7EE] p-2 rounded-md"
+                  : "text-[#fff]"
+              }
+              onClick={() => allproduct()}
+            >
+              All Products
+            </button>
+            <button
+              className={
+                selected === true
+                  ? "bg-[#6F757C] text-[#FEF7EE] p-2 rounded-md"
+                  : "text-[#fff]"
+              }
+              onClick={() => availableCofee()}
+            >
+              Available Now
+            </button>
+          </div>
+          <Product coffee={coffee} />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/Product.jsx
@@ -0,0 +1,36 @@
+import starfill from "../assets/Star_fill.svg";
+import unfill from "../assets/Star.svg";
+
+const Product = ({ coffee }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3  md:gap-x-4  my-4">
+      {coffee.map((data, index) => {
+        return (
+          <div key={index} className="py-3">
+            {data.popular === true ? <span className="bg-[#F6C768] rounded-full px-3 mx-2 my-1  absolute"> popular  </span> : ""}
+            <img
+              src={data.image}
+              alt={data.name}
+              className="h-40 w-56 lg:w-full  object-cover rounded-lg"
+            />
+            <div className="flex items-center justify-between pt-3">
+              <h2 className="text-white text-lg">{data.name}</h2>
+              <h4 className="bg-[#BEE3CC] py-1 mr-2 px-1 rounded">
+                {data.price}
+              </h4>
+            </div>
+            <p className="flex space-x-1 lg:space-x-5 items-center mt-3">
+              {data.votes > 1 ? <img src={starfill} /> : <img src={unfill} />}
+              <span className="text-[#FEF7EE]">{data.rating}</span>
+              <span className="text-[#6F757C] mx-2">({data.votes} Votes)</span>
+              {data.available === false ? <span className="text-[#ED735D]">Sold Out</span> : ""}
+              <span>{data.sold}</span>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Product;
